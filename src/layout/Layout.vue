6320362a4c9fdd4f0fd1a627fac787fe10c3b93b
jiangliangtong <jiangliangtong@gmail.com>
2025-08-26 09:10:53 +0800

    feat: update

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -52,7 +52,6 @@ import Player from './Player.vue';
 .header {
   height: 50px;
   padding: 0;
-  box-shadow: 0px 1px 4px rgba(0, 21, 41, 0.08);
 }
 
 .sidebar {
@@ -67,6 +66,5 @@ import Player from './Player.vue';
 .footer {
   height: 70px;
   padding: 0;
-  box-shadow: 0 -1px 4px rgba(0, 21, 41, 0.08);
 }
 </style>
