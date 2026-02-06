86c03190b8b567cea72faa7da08d5d64732ce51c
jiangliangtong <jiangliangtong@gmail.com>
2026-02-06 17:33:21 +0800

    feat: init

--- lib/api/music_api.dart
+++ lib/api/music_api.dart
@@ -22,20 +22,24 @@ class MusicApi {
         final device = deviceJson != null ? jsonDecode(deviceJson) : null;
         final userInfo = userInfoJson != null ? jsonDecode(userInfoJson) : null;
 
-        final authParts = <String>[];
-        
-        if (userInfo != null) {
-          if (userInfo['token'] != null) authParts.add('token=${userInfo['token']}');
-          if (userInfo['userid'] != null) authParts.add('userid=${userInfo['userid']}');
-          if (userInfo['t1'] != null) authParts.add('t1=${userInfo['t1']}');
-        }
+        final token = userInfo?['token'];
+        final userid = userInfo?['userid'];
+        final t1 = userInfo?['t1'];
+        final dfid = device?['dfid'];
+        final mid = device?['mid'];
+        final guid = device?['guid'];
+        final serverDev = device?['serverDev'];
+        final mac = device?['mac'];
 
-        if (device != null) {
-          if (device['mid'] != null) authParts.add('KUGOU_API_MID=${device['mid']}');
-          if (device['guid'] != null) authParts.add('KUGOU_API_GUID=${device['guid']}');
-          if (device['serverDev'] != null) authParts.add('KUGOU_API_DEV=${device['serverDev']}');
-          if (device['mac'] != null) authParts.add('KUGOU_API_MAC=${device['mac']}');
-        }
+        final authParts = <String>[];
+        if (token != null) authParts.add('token=$token');
+        if (userid != null) authParts.add('userid=$userid');
+        if (dfid != null) authParts.add('dfid=$dfid');
+        if (t1 != null) authParts.add('t1=$t1');
+        if (mid != null) authParts.add('KUGOU_API_MID=$mid');
+        if (guid != null) authParts.add('KUGOU_API_GUID=$guid');
+        if (serverDev != null) authParts.add('KUGOU_API_DEV=$serverDev');
+        if (mac != null) authParts.add('KUGOU_API_MAC=$mac');
 
         if (authParts.isNotEmpty) {
           options.headers['Authorization'] = authParts.join(';');
@@ -64,7 +68,7 @@ class MusicApi {
 
   static Future<Map<String, dynamic>?> registerDevice() async {
     try {
-      final response = await _dio.get('/register/dev', queryParameters: {'register': 'true'});
+      final response = await _dio.get('/register/dev');
       if (response.data['status'] == 1) {
         return response.data['data'];
       }
