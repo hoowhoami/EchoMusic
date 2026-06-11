8a82a192ae12a3fe19b95ba126073f121de1aeab
whoami <jiangliangtong@gmail.com>
2026-06-11 10:10:42 +0800

    feat: add ffmpeg player

--- native/echo-ffmpeg-player/build.rs
+++ native/echo-ffmpeg-player/build.rs
@@ -1,21 +1,3 @@
 fn main() {
     napi_build::setup();
-
-    #[cfg(target_os = "linux")]
-    {
-        use std::{env, fs, path::PathBuf};
-
-        let out_dir = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR should be set"));
-        let exports = out_dir.join("echo-ffmpeg-player.exports");
-        fs::write(
-            &exports,
-            b"{\n  global:\n    napi_register_module_v1;\n  local:\n    *;\n};\n",
-        )
-        .expect("failed to write Linux export map");
-
-        println!(
-            "cargo:rustc-link-arg-cdylib=-Wl,--version-script={}",
-            exports.display()
-        );
-    }
 }
